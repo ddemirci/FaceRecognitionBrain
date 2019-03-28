fa027171c76f0dec677e42e945e850a855c0f272
Doruk Demirci <dorukdemirci.boun@gmail.com>
2019-03-29 00:33:30 +0300

    ImageLinkForm and Rank components have been implemented. Particles.js was isntalled for background

---
+++ face-recognition-brain-app/src/components/Rank/Rank.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Rank.css';
+
+const Rank = () => {
+  return (
+    <div>
+      <div class="white f3">{'Hey user! Your current rank is...'}</div>
+      <div class="white f1">{'#NUM'}</div>
+    </div>
+  );
+};
+
+export default Rank;
