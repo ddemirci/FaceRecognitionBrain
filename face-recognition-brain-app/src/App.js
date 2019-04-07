751e5dc33cf8f391a660d90b4f21aeb9998163b8
Doruk Demirci <dorukdemirci.boun@gmail.com>
2019-04-08 00:45:51 +0300

    Face Recognition API has been configured.

--- face-recognition-brain-app/src/App.js
+++ face-recognition-brain-app/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecoginiton from './components/FaceRecoginiton/FaceRecoginiton';
+import Clarifai from 'clarifai';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Particles from 'react-particles-js'
 import Rank from './components/Rank/Rank';
 
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '3850e190f0504aae85707e177bcf0b17'
+});
+
 const particlesOptions={
     particles: {
       number:{
@@ -20,6 +27,31 @@ const particlesOptions={
 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      input:'',
+      imageUrl:''
+    }
+  }
+
+ onInputChanged = (event) =>{
+   this.setState({input: event.target.value});
+ }
+
+ onButtonSubmitted = () =>{
+   this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,
+      this.state.input).then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+ }
+
   render() {
     return (
       <div className="App">
@@ -29,8 +61,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecoginiton /> */}
+        <ImageLinkForm onInputChange={this.onInputChanged} onButtonSubmit={this.onButtonSubmitted}/>
+        <FaceRecoginiton imageUrl={this.state.imageUrl} />
       </div>
     );
   }
